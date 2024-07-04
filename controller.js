1737f68e2c8d46c07bba06c9880a681dfa493210
ana cenali <cenali.ana@alunoescolassociais.org.brm>
2024-07-04 11:04:40 -0300

    adiciona onclick

---
+++ controller.js
@@ -0,0 +1,15 @@
+//FUNÇÃO PARA VALIDAÇÃO DE ACESSO
+
+function acessar() {
+    let loginEmail = document.getElementById
+        ('loginEmail').value;
+    let loginSenha = document.getElementById
+        ('loginSenha').value;
+
+    if (loginEmail || !loginSenha) {
+        alert("Favor preencher todos os campos");
+    } else {
+        alert("Campos preenchidos com sucesso");
+    }
+
+}
