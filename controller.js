365ec847c8e7c419ccc9d3c5e923e00cf94e9cd3
ana cenali <cenali.ana@alunoescolassociais.org.brm>
2024-07-04 11:56:42 -0300

    adiciona mensagem

--- controller.js
+++ controller.js
@@ -1,21 +1,25 @@
 //FUNÇÃO PARA VALIDAÇÃO DE ACESSO
 
 function acessar() {
-    let loginEmail = document.getElementById
-        ('loginEmail').value;
-    let loginSenha = document.getElementById
-        ('loginSenha').value;
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
 
     if (!loginEmail || !loginSenha) {
         alert("Favor preencher todos os campos");
     } else {
-       // alert("Campos preenchidos com sucesso");
-       window.location.href = 'cadastro.html'
+        // alert("Campos preenchidos com sucesso");
+        window.location.href = 'cadastro.html'
     }
 
 }
 // FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY 
 var dadoLista = [];
-function salvarUser(){
-    
+function salvarUser() {
+    let nomeUser = document.getElementById('value').value;
+
+    if (nomeUser) {
+
+    }else{
+        alert("Favor informe um nome para cadastro")
+    }
 }
