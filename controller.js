026ae209e6708b563d1aa9aacedeef707a590c93
ana cenali <cenali.ana@alunoescolassociais.org.brm>
2024-08-09 08:36:54 -0300

    Comentando código if e else

--- controller.js
+++ controller.js
@@ -1,19 +1,15 @@
 // Função acessar define uma função, que é responsável por validar o preenchimento dos campos de login e realizar uma ação com base nessa validação.
 function acessar(){
 
-// loginEmail e loginSenha são variáveis que armazenam os valores dos campos de entrada de email e senha, respectivamente. A função document.getElementById().value é usada para pegar o valor atual 
-// desses campos de entrada no HTML.
+// loginEmail e loginSenha são variáveis que armazenam os valores dos campos de entrada de email e senha, respectivamente. A função document.getElementById().value é usada para pegar o valor atual desses campos de entrada no HTML.
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
  
-    // A instrução if (!loginEmail || !loginSenha) verifica se qualquer um dos campos está vazio.
-    // O operador || é um operador lógico "OU", o que significa que se qualquer uma das condições for verdadeira 
-    // (ou seja, se qualquer campo estiver vazio), a condição geral será verdadeira.
+// A instrução verifica se qualquer um dos campos está vazio. 
+// O operador || é um operador lógico "OU", o que significa que se qualquer campo estiver vazio, a condição geral será verdadeira.
     if(!loginEmail || !loginSenha){
-        alert("Favor preencher todos os campos.")  // Se a condição do if for verdadeira (ou seja, se algum campo estiver vazio),
-        // um alerta é exibido ao usuário, pedindo para preencher todos os campos.
+        alert("Favor preencher todos os campos.")  // Se a condição do if estiver vazio, um alerta é exibido ao usuário, pedindo para preencher todos os campos.
        
-
     }else{
         // alert("Campos preenchidos com sucesso!");    Linha criada para teste.
         window.location.href = 'cadastro.html';
