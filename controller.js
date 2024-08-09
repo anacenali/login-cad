fa05fcb9384d61b9778c8a29edc13224e85dba6e
ana cenali <cenali.ana@alunoescolassociais.org.brm>
2024-08-09 08:08:19 -0300

    Cria javaScript

--- controller.js
+++ controller.js
@@ -1,25 +1,50 @@
-//FUNÇÃO PARA VALIDAÇÃO DE ACESSO
-
-function acessar() {
+// Função para validar o acesso.
+ 
+function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
-
-    if (!loginEmail || !loginSenha) {
-        alert("Favor preencher todos os campos");
-    } else {
-        // alert("Campos preenchidos com sucesso");
-        window.location.href = 'cadastro.html'
-    }
-
-}
-// FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY 
-var dadoLista = [];
-function salvarUser() {
-    let nomeUser = document.getElementById('nomeUser').value;
-
-    if (nomeUser) {
-
+ 
+    if(!loginEmail || !loginSenha){
+        alert("Favor preencher todos os campos.")
     }else{
-        alert("Favor informe um nome para cadastro");
+        // alert("Campos preenchidos com sucesso!");    
+        window.location.href = 'cadastro.html';
     }
+}
+ 
+// Função para armazenar os nomes em ARRAY
+var dadosLista = []; 
+ 
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+ 
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        //console.log(dadosLista);  
+        criaLista();
+        document.getElementById('nomeUser').value = ""; 
+    }else{
+        alert("Favor, informe um nome para cadastro.");
+    }
+}
+// Função para criar uma lista de usuários
+function criaLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário.</th><th>Ações.</th></tr>"; 
+    for(let i=0;i <= (dadosLista.length -1);i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>"; 
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+ 
+}
+ 
+//Função para editar os nomes das listas.
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1)   
+}
+ 
+// Função para excluir o nome da lista.
+function excluir(i){
+    dadosLista.splice((i - 1), 1); 
+    document.getElementById('tabela').deleteRow(i);
 }
